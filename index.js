77a8fe00ecaf475146801afe788396bbe49c2850
sidee <said.issack@student.moringaschool.com>
2023-06-13 23:41:08 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+    // Calculates the distance from headquarters in blocks.
+    return blockNumber;
+  }
+  
+  function distanceFromHqInFeet(blockNumber) {
+    // Calculates the distance from headquarters in feet.
+    return blockNumber * 264;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    // Calculates the distance travelled in feet.
+    return Math.abs(start - destination) * 264;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    // Calculates the fare price.
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) 
+      return 0;
+    } else if (distance <= 2000) {
+      return distance * 0.02;
+    } else {
+      return 25;
+    }
+  
+  
