7b8ba626edc21df8c74a8fa5dd1669e50a707da3
sidee <said.issack@student.moringaschool.com>
2023-06-15 15:34:11 +0300

    completed Assighnment

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(blockNumber) {
-    // Calculates the distance from headquarters in blocks.
-    return blockNumber;
-  }
-  
-  function distanceFromHqInFeet(blockNumber) {
-    // Calculates the distance from headquarters in feet.
-    return blockNumber * 264;
-  }
-  
+function distanceFromHqInBlocks(blocks){
+  if (blocks >42) {
+    return blocks -42;
+ }else {
+   return 42 - blocks;
+ }}
+
+ function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks (blocks) * 264;
+ }
   function distanceTravelledInFeet(start, destination) {
-    // Calculates the distance travelled in feet.
-    return Math.abs(start - destination) * 264;
-  }
-  
-  function calculatesFarePrice(start, destination) {
-    // Calculates the fare price.
-    let distance = distanceTravelledInFeet(start, destination);
-    if (distance <= 400) 
-      return 0;
-    } else if (distance <= 2000) {
-      return distance * 0.02;
-    } else {
-      return 25;
-    }
-  
-  
+ if (start < destination ) {
+       return(destination - start) * 264;
+   } else{
+       return(start-destination) *264;}
+   }
+ 
+ function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+ 
+   if (distance <= 400) {
+       return 0;
+     } else if (distance > 400 && distance <=2000) {
+       return 0.02 * (distance-400);
+   } 
+   else if (distance > 2000 && distance < 2500) {
+     return 25;
+   } else {
+     return 'cannot travel that far';
+   } }
